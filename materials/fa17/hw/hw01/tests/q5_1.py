e0e10469a9db644c47e4c337ab96c677bd6c848e
naltieri <nl.altieri@gmail.com>
2017-08-26 13:29:34 -0700

    Horrible git edit to get things to push. Forgive me

--- materials/fa17/hw/hw01/tests/q5_1.py
+++ materials/fa17/hw/hw01/tests/q5_1.py
@@ -2,6 +2,20 @@ test = {
   'name': 'Question 5_1',
   'points': 1,
   'suites': [
-  
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(dissimilarity, (int, long, float))
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
   ]
-}
