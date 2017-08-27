6cb6bf9e195b4ce745f912c6c7166da082a3c9aa
David Wagner <daw@cs.berkeley.edu>
2017-08-26 18:40:31 -0700

    fa17/lab/lab02p.push

---
+++ materials/fa17/lab/lab02p/tests/q151.py
@@ -0,0 +1,22 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> import numpy as np
+          >>> discounted_prices == np.array([4., 4.8, 12.])
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/fa17/lab/lab02p/tests/q153.py
@@ -0,0 +1,31 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> len(error_array) == 4
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> # Check your math!
+          >>> multiples[3] == 8
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
