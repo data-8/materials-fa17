829040f6295ae65fde5617ad9ee32fdbbd16aa31
Alexander Ku <alexku@berkeley.edu>
2017-08-27 18:10:27 -0700

    fa17/hw/hw01e.push

---
+++ materials/fa17/hw/hw01e/tests/q5_1.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 5_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> isinstance(dissimilarity, (int, long, float))
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> dissimilarity
+          141
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
---
+++ materials/fa17/hw/hw01e/tests/q5_2.py
@@ -0,0 +1,30 @@
+test = {
+  'name': 'Question 5_2',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= universities_q2 <= 3
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+        {
+          'code': r"""
+          >>> universities_q2 == 2
+          True
+          """,
+          'hidden': True,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
