7f5f9b91187a770c9c0325810cc4dc1865c1d740
David Wagner <daw@cs.berkeley.edu>
2017-08-28 23:04:29 -0700

    Remove hidden tests.

--- materials/fa17/hw/hw01/tests/q2_1.py
+++ materials/fa17/hw/hw01/tests/q2_1.py
@@ -2,29 +2,21 @@ test = {
   'name': 'Question 2_1',
   'points': 1,
   'suites': [
+    {
+      'cases': [
         {
-          'cases': [
-            {
-              'code': r"""
-              >>> 1 <= characters_q1 <= 5
-              True
-              """,
-              'hidden': False,
-              'locked': False
-            },
-            {
-              'code': r"""
-              >>> characters_q1 == 1
-              True
-              """,
-              'hidden': True,
-              'locked': False
-            },
-          ],
-          'scored': True,
-          'setup': '',
-          'teardown': '',
-          'type': 'doctest'
+          'code': r"""
+          >>> 1 <= characters_q1 <= 5
+          True
+          """,
+          'hidden': False,
+          'locked': False
         }
-    ]
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
 }
