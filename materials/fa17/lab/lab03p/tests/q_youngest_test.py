e170d27667cd94532d1f043da423793c88ad28ec
naltieri <nl.altieri@gmail.com>
2017-09-05 03:38:24 -0700

    fa17/lab/lab03p.push

---
+++ materials/fa17/lab/lab03p/tests/q_youngest_test.py
@@ -0,0 +1,26 @@
+test = {
+  'name': '',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> youngest_upper == 0.92
+          True
+          >>> youngest_middle == 0.67
+          True
+          >>> youngest_upper > .419 and youngest_upper < .421
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
