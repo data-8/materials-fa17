2ecc495b72dd8bba732c2ea7e838a69e5adf9002
naltieri <nl.altieri@gmail.com>
2017-11-11 00:51:21 -0800

    fa17/hw/hw08.push

---
+++ materials/fa17/hw/hw08/tests/q1_6.py
@@ -0,0 +1,23 @@
+test = {
+  'name': 'Question 1_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure two_minutes_wait is a single value
+          >>> type(two_minutes_wait) == float
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
