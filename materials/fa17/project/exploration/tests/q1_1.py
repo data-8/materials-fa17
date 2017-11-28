4381856cbf12826801986372db7b5078213f9230
Alexander Ku <alexku@berkeley.edu>
2017-11-27 19:17:37 -0800

    fa17/project/exploration.push

---
+++ materials/fa17/project/exploration/tests/q1_1.py
@@ -0,0 +1,24 @@
+test = {
+  'name': 'Question 1_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> # Make sure that you name your submission file exploration.ipynb
+          >>> import os
+          >>> os.path.exists('exploration.ipynb')
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        },
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
